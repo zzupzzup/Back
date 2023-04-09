d8e7c0de151d320f6ad91d7ee8fc55d05813a0e1
HwangIsAce <esteen196@naver.com>
2023-04-09 19:08:28 +0900

    Feat: sign in by email

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,9 +1,7 @@
 from datetime import datetime, timedelta
-
 import bcrypt
 import jwt
 from fastapi import APIRouter, Depends
-
 # TODO:
 from sqlalchemy.orm import Session
 from starlette.responses import JSONResponse
@@ -11,9 +9,7 @@ from starlette.responses import JSONResponse
 from app.common.consts import JWT_SECRET, JWT_ALGORITHM
 from app.database.conn import db
 from app.database.schema import Users
-
 from models import SnsType, Token, UserToken, UserRegister
-
 """
 1. 구글 로그인을 위한 구글 앱 준비 (구글 개발자 도구)
 2. FB 로그인을 위한 FB 앱 준비 (FB 개발자 도구)
@@ -27,12 +23,8 @@ from models import SnsType, Token, UserToken, UserRegister
 10. 회원 탈퇴
 11. 탈퇴 회원 정보 저장 기간 동안 보유(법적 최대 한도차 내에서, 가입 때 약관 동의 받아야 함, 재가입 방지 용도로 사용하면 가능)
 """
-
-
 router = APIRouter()
-
-
-@router.post("/register/{sns_type}", status_code=200, response_model=Token)
+@router.post("/register/{sns_type}", status_code=201 , response_model=Token)
 async def register(sns_type: SnsType, reg_info: UserRegister, session: Session = Depends(db.session)):
     """
     회원가입 API
@@ -41,45 +33,45 @@ async def register(sns_type: SnsType, reg_info: UserRegister, session: Session =
     :param session:
     :return:
     """
-    # SnsType -> email 과 google 만 남기고 지우기
-    if sns_type == SnsType.email: # email 의 경우 # 진실이 -> 회원가입 창 만들라고 하기
+    
+    # sign up 관련 코드 # email sign up
+    # SnsType -> email 과 google 만 남기고 지우기 
+    if sns_type == SnsType.email: # 진실이 -> 회원가입 창 만들라고 전달
         is_exist = await is_email_exist(reg_info.email)
-        if not reg_info.email or not reg_info.pw: # email 과 pw 가 공란일 경우
+        if not reg_info.email or reg_info.pw: # email 과 pw 가 공란인 경우 
             return JSONResponse(status_code=400, content=dict(msg="Email and PW must be provided'"))
-        if is_exist: # 이미 기존에 존재하는 email 인 경우 
+        if is_exist: # 이미 기존에 존재하는 email 인 경우
             return JSONResponse(status_code=400, content=dict(msg="EMAIL_EXISTS"))
-        hash_pw = bcrypt.hashpw(reg_info.pw.encode("utf-8"), bcrypt.gensalt()) # pw 해시해서 저장할 목적
-        new_user = Users.create(session, auto_commit=True, pw=hash_pw, email=reg_info.email) # email 과 pw 를 DB 에 추가
+        hash_pw = bcrypt.hashpw(reg_info.pw.encode("utf-8"), bcrypt.gensalt()) # pw 해싱해서 저장할 목적 
+        new_user = Users.create(session, auto_commit=True, pw=hash_pw, email=reg_info.email) # email 과 pw 를 DB 에 저장
         token = dict(Authorization=f"Bearer {create_access_token(data=UserToken.from_orm(new_user).dict(exclude={'pw', 'marketing_agree'}),)}")
         return token
     return JSONResponse(status_code=400, content=dict(msg="NOT_SUPPORTED"))
 
 
+# login 관련 코드 # emain sign in 
 @router.post("/login/{sns_type}", status_code=200)
 async def login(sns_type: SnsType, user_info: UserRegister):
     if sns_type == SnsType.email:
         is_exist = await is_email_exist(user_info.email)
         if not user_info.email or not user_info.pw:
             return JSONResponse(status_code=400, content=dict(msg="Email and PW must be provided'"))
-        if not is_exist:
-            return JSONResponse(status_code=400, content=dict(msg="NO_MATCH_USER"))
+        if not is_exist: 
+            return JSONResponse(status_code=400, content=dict(msg="NO_MATCH_USER")) # DB 에 email 이 없을 때 NO_MATCH_USER 출력
         user = Users.get(email=user_info.email)
         is_verified = bcrypt.checkpw(user_info.pw.encode("utf-8"), user.pw.encode("utf-8"))
         if not is_verified:
-            return JSONResponse(status_code=400, content=dict(msg="NO_MATCH_USER"))
-        token = dict(
-            Authorization=f"Bearer {create_access_token(data=UserToken.from_orm(user).dict(exclude={'pw', 'marketing_agree'}),)}")
+            return JSONResponse(status_code=400, content=dict(msg="NO_MATCH_USER")) # pw 가 맞지 않을 때, NO_MATCH_USER 출력 
+        token = dict(Authorization=f"Bearer {create_access_token(data=UserToken.from_orm(user).dict(exclude={'pw', 'marketing_agree'}),)}")
         return token
     return JSONResponse(status_code=400, content=dict(msg="NOT_SUPPORTED"))
 
 
 async def is_email_exist(email: str):
-    get_email = Users.get(email=email)
+    get_email = Users.get(email=email) # get 는 schema.py 파일에 정의되어 있는데, DB 에 같은 메일이 2개 있으면 안되기 때문에 get 함수를 만들어서 사용
     if get_email:
         return True
     return False
-
-
 def create_access_token(*, data: dict = None, expires_delta: int = None):
     to_encode = data.copy()
     if expires_delta:

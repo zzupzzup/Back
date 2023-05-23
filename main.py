b206a827d5368730be4899a9fc5bdabf9eab57e5
HwangIsAce <esteen196@naver.com>
2023-05-23 17:14:24 +0900

    Feat: version:1.2

--- main.py
+++ main.py
@@ -43,10 +43,10 @@ def create_app():
     
     app.include_router(firstModel.router)
     app.include_router(chatRRS.router)
-    app.include_router(personalModel.router) # 개인화 추천 및 개인화 추천 store 상세페이지
+    # app.include_router(personalModel.router) # 개인화 추천 및 개인화 추천 store 상세페이지
     
     app.include_router(detail_page.router) # 상세 페이지
-    app.include_router(click_log.router) # click_log 
+    # app.include_router(click_log.router) # click_log 
     
     return app
 
